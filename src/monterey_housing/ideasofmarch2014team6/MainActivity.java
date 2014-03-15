21af959d2db6cf03d827e0d6236971b1fc97c3b1
nickzarzosa <nickolas@csumb.edu>
2014-03-15 12:26:35 -0700

    merge fix

--- src/monterey_housing/ideasofmarch2014team6/MainActivity.java
+++ src/monterey_housing/ideasofmarch2014team6/MainActivity.java
@@ -4,6 +4,35 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	//testing yeah...change 2!
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		// test
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	
+	
+	
+
+}
+=======
+package monterey_housing.ideasofmarch2014team6;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
 public class MainActivity extends Activity {
 
 	@Override
@@ -22,3 +51,4 @@ public class MainActivity extends Activity {
 	}
 
 }
+
