dedfa730ae062d146c54cc3cc505b7dcb5a36322
nickzarzosa <nickolas@csumb.edu>
2014-03-15 16:33:57 -0700

    fixed main

--- gen/monterey_housing/ideasofmarch2014team6/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package monterey_housing.ideasofmarch2014team6;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/monterey_housing/ideasofmarch2014team6/MainActivity.java
+++ src/monterey_housing/ideasofmarch2014team6/MainActivity.java
@@ -1,59 +1,5 @@
 package monterey_housing.ideasofmarch2014team6;
 
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-
-public class MainActivity extends ActionBarActivity {
-
-	@Override
-	//testing yeah...change 2!
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		// test
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	
-	
-	
-
-}
-=======
-package monterey_housing.ideasofmarch2014team6;
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-
-public class MainActivity extends Activity {
-
-	@Override
-	//testing yeah...change 2!
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		// test
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-}
-
-=======
 import java.util.ArrayList;
 
 import android.app.Activity;
@@ -64,7 +10,7 @@ import android.widget.ExpandableListView;
 
 public class MainActivity extends Activity {
   // more efficient than HashMap for mapping integers to objects
-	//this is a test
+	//
   SparseArray<Group> groups = new SparseArray<Group>();
 
   @Override
@@ -101,4 +47,4 @@ public class MainActivity extends Activity {
   }
 
 } 
->>>>>>> origin/develop
+
