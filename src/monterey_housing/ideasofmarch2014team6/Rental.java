bc40043c32804d18b5f19b31149352386ee8fae9
jebutton <jebutton@gmail.com>
2014-03-15 16:26:19 -0700

    upload of filled out rental class

--- src/monterey_housing/ideasofmarch2014team6/Rental.java
+++ src/monterey_housing/ideasofmarch2014team6/Rental.java
@@ -32,4 +32,108 @@ public class Rental {
 		this.phoneNum = phoneNum;
 		this.otherInfo = otherInfo;
 	}
+	
+	//-------------
+	//mutator methods
+	//------------
+	
+	/**
+	 * sets the city
+	 * @param city
+	 */
+	public void setCity(String city){
+		this.city = city;
+	}
+	
+	/**
+	 * sets the address
+	 * @param address
+	 */
+	public void setAddress(String address){
+		this.address = address;
+	}
+	
+	/**
+	 * sets the number of bedrooms
+	 * @param numBedrooms
+	 */
+	public void setNumBedrooms(String numBedrooms){
+		this.numBedrooms = numBedrooms;
+	}
+	/**
+	 * sets the rent 
+	 * @param rent
+	 */
+	public void setRent(String rent){
+		this.rent = rent;
+	}
+	
+	/**
+	 * sets the phone number
+	 * @param phoneNum
+	 */
+	public void setPhoneNum(String phoneNum){
+		this.phoneNum = phoneNum;
+	}
+	
+	/**
+	 * sets the other info field
+	 * @param otherInfo
+	 */
+	public void setOtherInfo(String otherInfo){
+		this.otherInfo = otherInfo;
+	}
+	
+	//-------------
+	// accessor methods
+	//--------------
+	
+	/**
+	 * returns the city
+	 * @return city
+	 */
+	public String getCity(){
+		return this.city;
+	}
+	
+	/**
+	 * returns the address
+	 * @return address
+	 */
+	public String getAddress(){
+		return this.address;
+	}
+	
+	/**
+	 * returns number of bedrooms
+	 * @return numBedrooms
+	 */
+	public String getNumBedrooms(){
+		return this.numBedrooms;
+	}
+	
+	
+	/**
+	 * returns the rent
+	 * @return rent
+	 */
+	public String getRent(){
+		return this.rent;
+	}
+	
+	/**
+	 * returns the phone number
+	 * @return phoneNum
+	 */
+	public String getPhoneNum(){
+		return this.phoneNum;
+	}
+	
+	/**
+	 * returns the other info field
+	 * @return otherInfo
+	 */
+	public String getOtherInfo(){
+		return this.otherInfo;
+	}
 }
