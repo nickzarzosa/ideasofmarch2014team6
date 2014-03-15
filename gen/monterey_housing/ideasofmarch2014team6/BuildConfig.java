56b86c80f656d1ae64f65fceb397f9a6f9951faf
jjflores <shadowdragon19@gmail.com>
2014-03-14 20:49:57 -0700

    Added listings layout

---
+++ gen/monterey_housing/ideasofmarch2014team6/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package monterey_housing.ideasofmarch2014team6;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
