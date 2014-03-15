8240d04c5908743dfe5ef315c4ebd7cce8586eb1
justkeating <kkeating90@yahoo.com>
2014-03-15 10:19:39 -0700

    favorites page
    added favorites xml page

--- gen/monterey_housing/ideasofmarch2014team6/R.java
+++ gen/monterey_housing/ideasofmarch2014team6/R.java
@@ -28,24 +28,37 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000d;
-        public static final int checkBox1=0x7f090002;
-        public static final int checkBox2=0x7f090005;
-        public static final int checkBox3=0x7f090008;
-        public static final int checkBox4=0x7f09000b;
-        public static final int resultsListRow1=0x7f090001;
-        public static final int resultsListRow2=0x7f090004;
-        public static final int resultsListRow3=0x7f090007;
-        public static final int resultsListRow4=0x7f09000a;
-        public static final int tableLayout1=0x7f090000;
-        public static final int textView1=0x7f090003;
-        public static final int textView2=0x7f090006;
-        public static final int textView3=0x7f090009;
-        public static final int textView4=0x7f09000c;
+        public static final int RelativeLayout1=0x7f090000;
+        public static final int action_settings=0x7f090019;
+        public static final int button1=0x7f090004;
+        public static final int button2=0x7f090006;
+        public static final int button3=0x7f090009;
+        public static final int button6=0x7f090007;
+        public static final int button7=0x7f09000a;
+        public static final int cbutton0=0x7f090003;
+        public static final int checkBox1=0x7f09000e;
+        public static final int checkBox2=0x7f090011;
+        public static final int checkBox3=0x7f090014;
+        public static final int checkBox4=0x7f090017;
+        public static final int favoriteTextView=0x7f09000b;
+        public static final int favoritesTextView=0x7f09000f;
+        public static final int resultsListRow1=0x7f09000d;
+        public static final int resultsListRow2=0x7f090010;
+        public static final int resultsListRow3=0x7f090013;
+        public static final int resultsListRow4=0x7f090016;
+        public static final int tableLayout1=0x7f090001;
+        public static final int tableRow1=0x7f090002;
+        public static final int tableRow2=0x7f090005;
+        public static final int tableRow3=0x7f090008;
+        public static final int tbl_test=0x7f09000c;
+        public static final int textView2=0x7f090012;
+        public static final int textView3=0x7f090015;
+        public static final int textView4=0x7f090018;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int ha_listview=0x7f030001;
+        public static final int ha_favorites=0x7f030001;
+        public static final int ha_listview=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
