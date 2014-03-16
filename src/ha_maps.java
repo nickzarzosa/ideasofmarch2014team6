0ee307d325c8cf97d23d8421f62ffe949cb493e2
justkeating <kkeating90@yahoo.com>
2014-03-15 17:44:48 -0700

    Maps

---
+++ src/ha_maps.java
@@ -0,0 +1,15 @@
+package monterey_housing.ideasofmarch2014team6;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ha_maps {
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_main);
+		}
+	
+
+}
