5da6e4b1e41c57d41a60141ea438efa5c1eb6243
jjflores <shadowdragon19@gmail.com>
2014-03-16 12:31:22 -0700

    all there activities working

--- src/monterey_housing/ideasofmarch2014team6/ListViewActivity.java
+++ src/monterey_housing/ideasofmarch2014team6/ListViewActivity.java
@@ -12,44 +12,52 @@ public class ListViewActivity extends Activity {
   // more efficient than HashMap for mapping integers to objects
 	
   SparseArray<Group> groups = new SparseArray<Group>();
-
+  int counter = 0;
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.ha_activity_list_view);
-    createData();
+    
+  	String[] values = new String[] { "Plaza Grande", "SALINAS", "SRO",
+  	        "SRO", "50 E MARKET ST", "771-972", "$450 - $540", "RSTSGW",
+  	        "Call to be put on waiting list. Deposit $650. Call Maribel Monday - Friday 10:00 - 4:30pm."};
+    createData(values);
+    
+    String[] values2 = new String[] { "MARCUS MORGAN", "GREENFIELD", "1",
+  	        "ROOM", "226 SAN ANTONIO DR", "415-410-4408", "$400", "RSTSGWGE",
+  	        "Available Dec 1. Deposit 400. Call mornings."};
+    createData(values2);
+    
+    String[] values3 = new String[] { "EVE", "GREENFIELD", "0",
+  	        "STUDIO", "", "676-6281", "$650", "RSTSGW",
+  	        "Available now. Deposit $650. Call for address"};
+    createData(values3);
+    
     ExpandableListView listView = (ExpandableListView) findViewById(R.id.listView);
     MyExpandableListAdapter adapter = new MyExpandableListAdapter(this,
         groups);
     listView.setAdapter(adapter);
   }
 
-  public void createData() {
-	String address = "Address \t";
-  	String bedroom = "Bedrooms \t";
-  	String rent = "Rent \t";
-  	String[] values = new String[] { "Plaza Grande", "SALINAS", "SRO",
-  	        "SRO", "50 E MARKET ST", "771-972", "$450 - $540", "RSTSGW",
-  	        "Call to be put on waiting list. Deposit $650. Call Maribel Monday - Friday 10:00 - 4:30pm."};
-  	int counter = 0;
+  public void createData(String[] values) {
+
+	
   	String string;
-    for (int i = 0; i < 5; i++) {
-//    	counter = 0;
-    	string = address+ values[4] +"\n"+ bedroom + values[2] + "\n" + rent + values[6];
-    	Group group = new Group(string);
-    	
-		group.children.add("Owner \t" + values[0]);
-		group.children.add(address + values[4]);
-		group.children.add("City \t" + values[1]);
-		group.children.add(rent + values[6]);
-		group.children.add(bedroom + values[2]);
-		group.children.add("Unit Type \t" + values[3]);
-		group.children.add("Phone \t" + values[5]);
-		group.children.add("Utilites \t" + values[7]);
-		group.children.add("OtherInfo \t" + values[8]);
-		groups.append(i, group);
-    }
+    
+	string = "Address \t" + values[4] +"\n"+ "Bedrooms \t" + values[2] + "\n" + "Rent \t" + values[6];
+	Group group = new Group(string);
+	
+	group.children.add("Owner \t" + values[0]);
+	group.children.add("Address \t" + values[4]);
+	group.children.add("City \t" + values[1]);
+	group.children.add("Rent \t" + values[6]);
+	group.children.add("Bedrooms \t" + values[2]);
+	group.children.add("Unit Type \t" + values[3]);
+	group.children.add("Phone \t" + values[5]);
+	group.children.add("Utilites \t" + values[7]);
+	group.children.add("OtherInfo \t" + values[8]);
+	groups.append(counter, group);
+	counter++;
   }
-
-} 
-
+}
--- src/monterey_housing/ideasofmarch2014team6/MainActivity.java
+++ src/monterey_housing/ideasofmarch2014team6/MainActivity.java
@@ -1,25 +1,61 @@
 package monterey_housing.ideasofmarch2014team6;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.SparseArray;
-import android.view.Menu;
-import android.widget.ExpandableListView;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-  // more efficient than HashMap for mapping integers to objects
-	
-  SparseArray<Group> groups = new SparseArray<Group>();
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-//    DataPull testPull = new DataPull();
-    setContentView(R.layout.ha_activity_main);
+    private ListView listView1;
 
-  }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ha_activity_main);
+        buttonHandlers();
+    }
+    
+    
 
-} 
+    protected void buttonHandlers(){
+    	final Button showAll = (Button) findViewById(R.id.button0);
+    	final Button mapDirections = (Button) findViewById(R.id.button4);
+    	
+    	  showAll.setOnClickListener(new View.OnClickListener()
+    	    {
+    	    	public void onClick(View v){
+    	    		//start intnet
+    	    		openListViewActivity();    	    		
+    	    	}
+    	    	
+    	    });
+    	  
+    	  mapDirections.setOnClickListener(new View.OnClickListener()
+  	    {
+  	    	public void onClick(View v){
+  	    		//start intnet
+  	    		openHa_mapsActivity();    	    		
+  	    	}
+  	    	
+  	    });
 
+    }
+    
+    private void openListViewActivity(){
+    	 Intent intent = new Intent(this, ListViewActivity.class);
+    	 startActivity(intent);
+    }
+    
+    private void openHa_mapsActivity(){
+    	Intent intent = new Intent(this, ha_maps.class);
+   	 	startActivity(intent);
+    }
+}
