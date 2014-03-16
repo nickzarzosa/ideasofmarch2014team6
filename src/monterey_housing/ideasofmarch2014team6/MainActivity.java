601e5872383ffdf7518bb2143fed5e0ab94ff36d
jjflores <shadowdragon19@gmail.com>
2014-03-15 17:35:16 -0700

    Working master branch

--- src/monterey_housing/ideasofmarch2014team6/MainActivity.java
+++ src/monterey_housing/ideasofmarch2014team6/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_list_view);
     createData();
     ExpandableListView listView = (ExpandableListView) findViewById(R.id.listView);
     MyExpandableListAdapter adapter = new MyExpandableListAdapter(this,
